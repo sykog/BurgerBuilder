f72bbf0e380a04a7aa3fb6d8be31fc452130fe0f
sykog <asuarez2@mail.greenriver.edu>
2019-06-24 12:56:11 -0700

    ingredients are split into seperate tags, quantity is tracked

--- burger-builder/src/Components/Burger/Burger.js
+++ burger-builder/src/Components/Burger/Burger.js
@@ -3,13 +3,25 @@ import BurgerIngredient from './BurgerIngredient'
 import classes from './burger.css';
 
 const burger = props => {
+
+  // transform the ingredients object into an array of jsx tags
+  // reduce wil count each ingredient by quantity
+  let transformedIngredients = Object.keys(props.ingredients).map(ingredientName => {
+    return [...Array(props.ingredients[ingredientName])].map( (_, quantity)=> {
+      return <BurgerIngredient key={ingredientName + quantity} type={ingredientName}/>
+    });
+  }).reduce((ingredients, ingredient) => {
+    return ingredients.concat(ingredient)
+  }, []);
+  if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+  console.log(transformedIngredients);
+
   return(
     <div className={classes.burger}>
       <BurgerIngredient type="bunTop"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="lettuce"/>
-      <BurgerIngredient type="bacon"/>
-      <BurgerIngredient type="patty"/>
+      {transformedIngredients}
       <BurgerIngredient type="bunBottom"/>
     </div>
   );
--- burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react';
 import Burger from '../../Components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      lettuce: 0,
+      bacon: 0,
+      cheese: 0,
+      patty: 0
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
-        <Burger/>
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </React.Fragment>
     );
