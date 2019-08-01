df172f79fe1e05fe9c610308dc5d145286218582
sykog <asuarez2@mail.greenriver.edu>
2019-07-31 17:30:38 -0700

    new burger ingredient images, burger styling

--- burger-builder/src/Components/Burger/BurgerIngredient.js
+++ burger-builder/src/Components/Burger/BurgerIngredient.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import bunTop from '../../Assets/Images/bun-top.png';
+import bunBottom from '../../Assets/Images/bun-bottom.png';
+import patty from '../../Assets/Images/patty.png';
+import bacon from '../../Assets/Images/bacon.png';
+import lettuce from '../../Assets/Images/lettuce.png';
+import cheese from '../../Assets/Images/cheese.png';
 import classes from './burgerIngredients.css';
 
 const BurgerIngredient = props => {
@@ -7,27 +13,24 @@ const BurgerIngredient = props => {
 
   switch (props.type) {
     case('bunBottom'):
-      ingredient = <div className={classes.bunBottom}></div>;
+      ingredient = <img className={classes.ingredient} src={bunBottom} alt="bun bottom"/>;
       break;
     case('bunTop'):
       ingredient = (
-        <div className={classes.bunTop}>
-          <div className={classes.seeds1}></div>
-          <div className={classes.seeds2}></div>
-        </div>
+        <img className={classes.ingredient} src={bunTop} alt="bun top"/>
       );
       break;
     case('patty'):
-      ingredient = <div className={classes.patty}></div>;
+      ingredient = <img className={classes.ingredient} src={patty} alt="patty"/>;
       break;
     case('cheese'):
-      ingredient = <div className={classes.cheese}></div>;
+      ingredient = <img className={classes.ingredient} src={cheese} alt="cheese"/>;
       break;
     case('lettuce'):
-      ingredient = <div className={classes.lettuce}></div>;
+      ingredient = <img className={classes.ingredient} src={lettuce} alt="lettuce"/>;
       break;
     case('bacon'):
-      ingredient = <div className={classes.bacon}></div>;
+      ingredient = <img className={classes.ingredient} src={bacon} alt="bacon"/>;
       break;
     default:
       ingredient = null;
